c8b3f9513173dd9bb6a0db77dd220b9ea8efacaf
Phil Doise <p.doise@gmail.com>
2014-08-17 13:00:15 -0600

    seed work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,30 @@
+Recipe.delete_all
+
 Recipe.create([
-  { name: "Chicken and Sausage Jambalaya", contributor: "Phillip Doise" },
+  { name: "Chicken and Sausage Jambalaya", contributor: "Phillip Doise",
+  instructions: "Serving size: 6
+
+  Ingredients:
+  1 lbs smoked sausage sliced to 1/4 inch rounds
+  1 lbs chicken breast strips cubed
+  1 white onion chopped
+  4 cloves of garlic minced
+  1 1/2 cup uncooked white rice
+  3 1/3 cups of chicken broth or water
+  8 oz tomato sauce
+  Salt
+  Black pepper
+  Cayenne pepper
+  2 tsp Crab boil (not the liquid kind)
+  olive oil
+
+  Prepare:
+  1. Season sausage, chicken, onions and garlic with salt, pepper and cayenne pepper
+  2. Coat bottom of a cast iron pot with olive oil and set stove to med high
+  3. Cook onions and garlic until onions are turn clear. Then remove from pot.
+  4. Add chicken and lightly cook (will finish cooking during boil). The remove from pot.
+  5. Cook sausage then put chicken and vegetables back in pot and add broth, rice, tomato sauce, crab boil and add salt to taste.
+  6. Bring to boil then set to low heat, cover pot and let sit for 30 to 40 mins until rice is cooked" },
   { name: "Crawfish Fettuccine", contributor: "Phillip Doise" },
   { name: "Crawfish Etouffee", contributor: "Phillip Doise" },
   { name: "Seafood Gumbo", contributor: "Phillip Doise" },
