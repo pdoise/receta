4cd985f2f7ac9e514445c8e0a688916cfb4a4cfb
Phil Doise <p.doise@gmail.com>
2014-08-16 19:55:02 -0600

    show all recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,7 @@ class RecipesController < ApplicationController
     @recipes = if params[:keywords]
                  Recipe.where('name ilike ?',"%#{params[:keywords]}%")
                else
-                 []
+                 Recipe.all
                end
   end
 
