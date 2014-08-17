a5fd1dc4050a515170d1d5d074d31596f452b89d
Phil Doise <p.doise@gmail.com>
2014-08-17 13:43:07 -0600

    seed work

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,75 @@ Recipe.create([
   4. Add chicken and lightly cook (will finish cooking during boil). The remove from pot.
   5. Cook sausage then put chicken and vegetables back in pot and add broth, rice, tomato sauce, crab boil and add salt to taste.
   6. Bring to boil then set to low heat, cover pot and let sit for 30 to 40 mins until rice is cooked" },
-  { name: "Crawfish Fettuccine", contributor: "Phillip Doise" },
-  { name: "Crawfish Etouffee", contributor: "Phillip Doise" },
+  { name: "Crawfish Fettuccine", contributor: "Phillip Doise", instructions: "Ingredients:
+    1 stick of butter
+    1 onion chopped
+    1 bell pepper chopped
+    1 cup diced celery
+    3 cloves garlic minced
+    2 tbs flour
+    1 lbs crawfish
+    8 oz velveta
+    1 cup heavy whipping cream
+    cajun seasoning
+    cayenne pepper
+    1 lbs fettuccine pasta
+    1/2 cup parmesan
+
+    Prepare:
+    1. Melt butter in a large skillet over medium heat
+    2. Cook vegetables untill onions are soft
+    3. Stir in flour and cook a minute
+    4. Add crawfish and cook until hot
+    5. Add velveta and seasonings
+    6. Cover and simmer for 20 mins stirring occasionally
+    7. Meanwhile boil fettuccine and drain
+    8. Preheat oven to 350
+    9. Butter baking pan, stir in fettuccine and crawfish, sprinkle parmesan on top
+    10. Bake for about 30 mins"},
+  { name: "Crawfish Etouffee", contributor: "Phillip Doise", instructions: "Ingredients:
+    1/2 an onion chopped
+    1/2 a bell pepper chopped
+    few sticks of celery chopped
+    1/4 cup chopped green onion
+    3 garlic cloves minced
+    3 tbs tomato paste
+    22 ounces condensed cream of mushroom (or chicken) soup
+    2 lbs crawfish
+    Cajun seasoning
+    Salt
+    Pinch of crab boil
+
+    Prepare:
+    1. Cook vegetable until onions are soft
+    2. add soup, crawfish and seasonings
+    3. Cook about 20 stirring occasionally" },
   { name: "Seafood Gumbo", contributor: "Phillip Doise" },
+  { name: "La Casa Salsa", contributor: "Jana Griffin", instructions: "Ingredients:
+    Olive oil
+    1 bunch of green onions
+    1 tsp minced garlic
+    1 tbs chopped jalapenos
+    2 cans diced tomatoes
+    1 can Rotel
+    Salt
+
+    Prepare:
+    Cook the green onions down with a little olive oil. Add garlic and jalapenos, cook 5 mins. Add tomatoes and rotel, cook 30 more minutes. Add salt to taste. Add water as needed."},
+  { name: "Crawfish Casserole", contributor: "Jana Griffin", instructions: "Ingredients:
+    1 onion
+    1 bell pepper
+    1/2 to 1 stick of butter
+    1 can Rotel
+    1 can of cream of mushroom soup
+    1 can of cream of onion soup
+    2 lbs crawfish
+    Garlic powder
+    Tony's or Buddy's
+    Liquid crab boil
+    2 cups of rice
+    shredder cheese
+
+    Prepare:
+    Saute onion and bell pepper in butter. Add all other ingredients, mix well, pour in casserole dish. Cover and bake at 400 degrees for 45 minutes-1 hour (stir halfway through). Then uncover, sprinkle cheese on top, and bake for 5 minutes."}
 ])
