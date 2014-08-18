63114779b94a7b60930cf43e62f98ecb1e8b89d3
Phil Doise <p.doise@gmail.com>
2014-08-18 14:07:59 -0600

    seed work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Recipe.delete_all
 
 Recipe.create([
-  { name: "Chicken and Sausage Jambalaya", contributor: "Phillip Doise",
+  { name: "Chicken and Sausage Jambalaya", contributor: "Phillip",
   instructions: "Serving size: 6
 
   Ingredients:
@@ -26,7 +26,7 @@ Recipe.create([
   5. Cook sausage then put chicken and vegetables back in pot and add broth, rice, tomato sauce, crab boil and add salt to taste.
   6. Bring to boil then set to low heat, cover pot and let sit for 30 to 40 mins until rice is cooked" },
 
-  { name: "Crawfish Fettuccine", contributor: "Phillip Doise", instructions: "Ingredients:
+  { name: "Crawfish Fettuccine", contributor: "Phillip", instructions: "Ingredients:
     1 stick of butter
     1 onion chopped
     1 bell pepper chopped
@@ -53,7 +53,7 @@ Recipe.create([
     9. Butter baking pan, stir in fettuccine and crawfish, sprinkle parmesan on top
     10. Bake for about 30 mins"},
 
-  { name: "Crawfish Etouffee", contributor: "Phillip Doise", instructions: "Ingredients:
+  { name: "Crawfish Etouffee", contributor: "Phillip", instructions: "Ingredients:
     1/2 an onion chopped
     1/2 a bell pepper chopped
     few sticks of celery chopped
@@ -73,9 +73,9 @@ Recipe.create([
     3. Cook about 20 stirring occasionally
     4. Serve on rice"},
 
-  { name: "Seafood Gumbo", contributor: "Phillip Doise" },
+  { name: "Seafood Gumbo", contributor: "Phillip" },
 
-  { name: "La Casa Salsa", contributor: "Jana Griffin", instructions: "Ingredients:
+  { name: "La Casa Salsa", contributor: "Jana", instructions: "Ingredients:
     Olive oil
     1 bunch of green onions
     1 tsp minced garlic
@@ -87,7 +87,30 @@ Recipe.create([
     Prepare:
     Cook the green onions down with a little olive oil. Add garlic and jalapenos, cook 5 mins. Add tomatoes and rotel, cook 30 more minutes. Add salt to taste. Add water as needed."},
 
-  { name: "Crawfish Casserole", contributor: "Jana Griffin", instructions: "Ingredients:
+  { name: "Andouille Cream Cheese Dip", contributor: "Mike", instructions:
+    "You can add mashed avocado to make this a green dip if you like.
+    1/2 lb andouille, chopped
+    1 brick cream cheese
+    4 oz sour cream (we used "greek" sour cream)
+    Magic Swamp Dust Fire Department Blend spicy cajun
+    seasoning several tablespoons slice green onions (tops only)
+    hot sauce to taste
+    Brown the andouille until crispy. Soften the cream cheese while you're doing that. Then mix everything together. Sprinkle an extra bit of cajun seasoning on top of the dip, add a few green onion slices on top of that."},
+
+  { name: "Cajun Corn & Shrimp Bisque", contributor: "Mike", instructions:
+    "Yield : 12 servings
+    1 Large Onion 3
+    16oz cans of kernel corn, drained
+    2 lbs. Peeled Shrimp
+    3 Tbsp. Butter
+    2 (14oz) cans of Chicken Broth
+    1 1/2 cups of Half & Half
+    1 (8oz.) Soft Cream Cheese
+    3 Tbsp. cornstarch (mix with 1/2 cup of cold water)
+    Cooking:
+    Peel Onion and puree‐set inside. Semi‐puree one can of corn‐set aside. Take about a cup of shrimp and semi‐puree (this will set a flavor throughout the bisque)‐set aside. In a large pot ,melt butter and sauté onions. Add in one tbsp. of Down The Bayou Seasoning, cream cheese‐ Cook until cream cheese melts down making sure there are no lumps. Add in chicken broth, half & half, shrimp and corn. Bring to a boil then reduce to a simmer for approximately 25‐30 minutes, stirring occasionally. Mix corn starch and water‐pour into pot, continue stirring until mixture thickens. While the pot is simmering, it is a good time to adjust the seasoning. This will be a spicy bisque and should have some bite to it. Take 1‐1/2 tsp of Down The Bayou Seasoning ‐stir in slowly til dissolved."},
+
+  { name: "Crawfish Casserole", contributor: "Jana", instructions: "Ingredients:
     1 onion
     1 bell pepper
     1/2 to 1 stick of butter
@@ -103,4 +126,41 @@ Recipe.create([
 
     Prepare:
     Saute onion and bell pepper in butter. Add all other ingredients, mix well, pour in casserole dish. Cover and bake at 400 degrees for 45 minutes-1 hour (stir halfway through). Then uncover, sprinkle cheese on top, and bake for 5 minutes."}
+
+  { name: "Cajun Crab and Mushroom Bisque", contributor: "Mike", instructions:
+    "1 medium onion, diced
+    4 tablespoons butter
+    1 pound mushrooms, diced
+    4 tablespoons flour
+    3 cups chicken broth
+    1 cup milk
+    1 cup whipping cream
+    1/4 tablespoon parsley
+    Green Onion tops for garnish
+    1 pound crabmeat or Crab fingers
+    Add Down The Bayou Cajun Seasoning to taste
+
+    Melt butter and add onions sautee until onions look clear. Add mushrooms. Cook on low heat for 10 minutes. Add all other Ingredients, except green onion tops. Simmer on low heat for 30 minutes. Do not boil. Garnish with onion tops."}
+
+  { name: "Catfish Dallas", contributor: "Mike", instructions:
+    "For the fish:
+    catfish fillets
+    salt and pepper
+    1/4 c olive oil
+
+    Heat a cast iron skillet over a low‐medium flame. Salt and pepper the fillets. Add the olive oil to the pan, when it is heated, place the fillets in the pan. Cook for about 2‐3 minutes (depending on the thickness of the fish) and then flip it over and cook for another 2 minutes, or until fish flakes easily with a fork. Remove to a warm plate and serve with topping:
+
+    For the topping:
+    2 ‐3 tbsp olive oil
+    1/2 small onion, julienned
+    1/2 small bell pepper, julienned
+    1/2 lemon, sliced
+    1/2 c small shrimp, peeled and deveined
+    1 medium tomato, sliced into wedges
+    1/2 c pimiento stuffed green olives
+    pinch of cayenne pepper
+    1/2 c tomato sauce
+    chopped green onions
+
+    In a separate cast iron skillet, heat the olive oil over medium heat. Add the onions, bell peppers and lemon slices, stirring well to coat with oil. Cook 2‐3 minutes or until onions just begin to get soft. Then add shrimp and cayenne pepper. Cook one minute. Then add olives, tomatoes and tomato sauce. Stir gently until heated through. Remove from heat and spoon over fish. Garnish with green onions."}
 ])
