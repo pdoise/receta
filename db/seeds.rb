518f49a39c413d643d0f5eb9fa5cb6a6bd3c74f4
Phil Doise <p.doise@gmail.com>
2014-08-18 14:40:43 -0600

    seed work

--- db/seeds.rb
+++ db/seeds.rb
@@ -125,7 +125,7 @@ Recipe.create([
     shredder cheese
 
     Prepare:
-    Saute onion and bell pepper in butter. Add all other ingredients, mix well, pour in casserole dish. Cover and bake at 400 degrees for 45 minutes-1 hour (stir halfway through). Then uncover, sprinkle cheese on top, and bake for 5 minutes."}
+    Saute onion and bell pepper in butter. Add all other ingredients, mix well, pour in casserole dish. Cover and bake at 400 degrees for 45 minutes-1 hour (stir halfway through). Then uncover, sprinkle cheese on top, and bake for 5 minutes."},
 
   { name: "Cajun Crab and Mushroom Bisque", contributor: "Mike", instructions:
     "1 medium onion, diced
@@ -140,7 +140,7 @@ Recipe.create([
     1 pound crabmeat or Crab fingers
     Add Down The Bayou Cajun Seasoning to taste
 
-    Melt butter and add onions sautee until onions look clear. Add mushrooms. Cook on low heat for 10 minutes. Add all other Ingredients, except green onion tops. Simmer on low heat for 30 minutes. Do not boil. Garnish with onion tops."}
+    Melt butter and add onions sautee until onions look clear. Add mushrooms. Cook on low heat for 10 minutes. Add all other Ingredients, except green onion tops. Simmer on low heat for 30 minutes. Do not boil. Garnish with onion tops."},
 
   { name: "Catfish Dallas", contributor: "Mike", instructions:
     "For the fish:
@@ -162,5 +162,92 @@ Recipe.create([
     1/2 c tomato sauce
     chopped green onions
 
-    In a separate cast iron skillet, heat the olive oil over medium heat. Add the onions, bell peppers and lemon slices, stirring well to coat with oil. Cook 2‐3 minutes or until onions just begin to get soft. Then add shrimp and cayenne pepper. Cook one minute. Then add olives, tomatoes and tomato sauce. Stir gently until heated through. Remove from heat and spoon over fish. Garnish with green onions."}
+    In a separate cast iron skillet, heat the olive oil over medium heat. Add the onions, bell peppers and lemon slices, stirring well to coat with oil. Cook 2‐3 minutes or until onions just begin to get soft. Then add shrimp and cayenne pepper. Cook one minute. Then add olives, tomatoes and tomato sauce. Stir gently until heated through. Remove from heat and spoon over fish. Garnish with green onions."},
+
+  { name: "Complete Steak Dinner in a Crockpot", contributor: "Mike", instructions:
+    "Prep time: 10 mins
+    Cook time: 7 hours
+    Total time: 7 hours 10 mins
+    Serves: 4
+
+    Ingredients:
+    2 Ribeye Steaks
+    1 bottle of Steak Sauce (I used Country Bobs)
+    Steak Seasoning of Choice (I used Rendezvous)
+    8-10 red potatoes
+    4 ears of corn
+    butter
+    Salt
+    Pepper
+
+    Instructions:
+    1. Place two (huge) Ribeye Steaks (or other cut of your choice) into the bottom of your crockpot.
+    2. Sprinkle to cover with steak seasoning of your choice
+    3. Pour ½ to ¾ cup steak sauce over steaks (I used almost a full bottle of Country Bobs All Purpose Sauce)
+    4. Take a sheet of foil and top over the steaks
+    5. Spritz the top with either cooking oil or Pam
+    6. Place a layer of red potatoes on the foil and sprinkle sea salt over
+    7. Add another layer of foil and cover the potatoes tightly to form a seal (this will allow the potatoes to steam
+    8. Take 4 ears of corn and add butter, salt and pepper and roll tightly in aluminum foil.
+    9. Place the ears of corn on top of the foil and then place the lid on tight.
+    10. Do not open the lid.
+    11. Cook for 6-7 hours on low"},
+
+  { name: "Crab Dip", contributor: "Mike", instructions:
+    "1 stick butter, divided
+    1 sweet onion, minced
+    1 container crab claw meat, drained and picked over for shells
+    1 can cream of mushroom soup
+    1 pint of heavy cream
+
+    In a skillet, melt half the butter and saute onions until soft. Add the rest of the butter and the crab meat. Stir in the soup and the cream, blending gently. Keep cooking over medium heat until the mixture thickens to desired consistency (may take several minutes, keep stirring gently so it doesn't scorch or separate). Serve with crackers."},
+
+  { name: "Crawfish Pie", contributor: "Mike", instructions:
+    "Ingerdients:
+    1 cup butter
+    1/2 cup flour
+    1 sweet onion, chopped
+    1 green bell pepper, chopped
+    celery, chopped
+    4 green onions, chopped
+    1/2 cup vegetable broth
+    3 tablespoons tomato paste
+    2 pounds cooked and peeled whole crawfish tails
+    3 cloves garlic, chopped
+    1/4 cup fresh parsley, chopped
+    Cajun seasoning to taste
+    hot sauce to taste
+    12 puff pastry shells
+
+    Directions
+    1. Melt butter in a large skillet on medium heat, then stir in flour; cook until light brown, about 10 minutes. Add sweet onion, green pepper, celery, and green onions. Cover and simmer on low heat for about one hour, stirring occasionally.
+    2.Stir in vegetable broth and tomato paste, and cook until it reaches a creamy consistency. Mix in crawfish tails, garlic, and parsley, then cook for about 15 minutes or until the tails are tender. Stir in Cajun seasoning and hot sauce.
+    3.Preheat an oven to 400 degrees F (200 degrees C). Bake the puff pastry shells for 5 to 7 minutes, or until they puff up and start to brown a little. Remove from oven and set aside to cool. Pull off center tops and save. Turn oven down to 350 degrees F (175 degrees C).
+    4.Spoon mixture into puff pastry shells until slightly overfilled. Put extra filling into a pie dish and cover with the remaining puff pastry shell center tops to create 6 small pies and 1 large crawfish pie. Place pies on a baking sheet.
+    5.Bake for about 15 minutes, or until the shells are golden brown and the filling has a nice shine to it."},
+
+  { name: "Crawfish Stuffed Bell Peppers", contributor: "Mike", instructions:
+    "Serves: 5
+    Ingredients:
+    1 lb. peeled crawfish tails, coarsely chopped
+    1 large onion, finely chopped 1 bell pepper, finely chopped
+    5 medium bell peppers, cut in half
+    1 cup celery, chopped
+    2 cloves garlic, finely chopped
+    1 Tbsp. parsley
+    1 1/2 sticks margarine
+    1/2 loaf stale French bread
+    1 egg 1/2 tsp.
+    salt 1/2 tsp.
+    Tabasco
+
+    Directions:
+    1. Melt margarine, Saute onions, bell pepper, celery and garlic
+    2. Until soft, onions should look clear
+    3. Soak bread in water then squeeze water out of bread
+    4. Add crawfish tails, parsley, and bread.
+    5. Add Tabasco and salt then mix well.
+    6. Remove skillet from burner and add egg blending well.
+    7. Boil bell pepper halves in water for 5 minutes. Remove from water and remove excess water.
+    8. Stuff bell peppers with mixture. Bake for 20 minutes"},
 ])
